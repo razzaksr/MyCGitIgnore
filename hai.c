e0cd5bc292b626e423dc8b9d01295f8de7e064ca
razzaksr <razzaksr@gmail.com>
2023-09-09 11:55:36 +0530

    first commit

---
+++ hai.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int alpha;
+    scanf("%d",&alpha);
+    printf("Hai there!!!!!! %d",alpha);
+    return 0;
+}
---
+++ saysome.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    double hai;
+    scanf("%lf",&hai);
+    printf("%lf is double value",hai);
+    return 0;
+}
